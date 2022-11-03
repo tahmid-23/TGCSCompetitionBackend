58cf1e37ed309185db6807d92668be62fee29b67
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-11-03 11:19:00 -0400

    nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
-import { createTGCSConnection } from './queries';
+import { createTGCSConnection } from './queries.js';
 
 dotenv.config();
 
