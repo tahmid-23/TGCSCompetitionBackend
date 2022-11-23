4c1229c84f52c3b3885901fd47bf25d319489dbc
tahmid-23 <60953955+tahmid-23@users.noreply.github.com>
2022-11-23 18:23:17 -0500

    add update and delete routes

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import express from 'express';
 import { createTGCSPool } from './connection.js';
 import { queryAllExperiences } from './queries/experience.js';
 import { queryAllSponsors } from './queries/sponsor.js';
-import { insert } from './queries/update.js';
+import { insert, remove, update } from './queries/update.js';
 
 dotenv.config();
 
@@ -24,6 +24,9 @@ app.use(
   })
 );
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/experiences', async (_req, res) => {
   const connection = await pool.getConnection();
   const experiences = await queryAllExperiences(connection);
@@ -36,9 +39,78 @@ app.get('/sponsors', async (_req, res) => {
   res.json(sponsors);
 });
 
-app.post('/insert', async (_req, res) => {
+export interface CustomRequest<T> extends Express.Request {
+  body: T;
+}
+
+export interface InsertData {
+  tableName: string;
+  data: Record<string, object>;
+}
+
+app.post('/insert', async (req: CustomRequest<InsertData>, res) => {
+  if (!req.body.tableName || !req.body.data) {
+    return;
+  }
+
   const connection = await pool.getConnection();
   await connection.beginTransaction();
+
+  insert(connection, req.body.tableName, req.body.data);
+
+  connection.commit();
+
+  res.sendStatus(200);
+});
+
+export interface UpdateData {
+  tableName: string;
+  tableId: string;
+  rowId: number;
+  data: Record<string, object>;
+}
+
+app.post('/update', async (req: CustomRequest<UpdateData>, res) => {
+  if (
+    !req.body.tableName ||
+    !req.body.tableId ||
+    !req.body.rowId ||
+    !req.body.data
+  ) {
+    return;
+  }
+
+  const connection = await pool.getConnection();
+  await connection.beginTransaction();
+
+  update(
+    connection,
+    req.body.tableName,
+    req.body.tableId,
+    req.body.rowId,
+    req.body.data
+  );
+
+  connection.commit();
+
+  res.sendStatus(200);
+});
+
+export interface RemoveData {
+  tableName: string;
+  tableId: string;
+  rowId: number;
+}
+
+app.post('/remove', async (req: CustomRequest<RemoveData>, res) => {
+  if (!req.body.tableName || !req.body.tableId || !req.body.rowId) {
+    return;
+  }
+
+  const connection = await pool.getConnection();
+  await connection.beginTransaction();
+
+  remove(connection, req.body.tableName, req.body.tableId, req.body.rowId);
 
   connection.commit();
 
--- src/queries/update.ts
+++ src/queries/update.ts
@@ -1,5 +1,17 @@
 import { Connection } from 'mysql2/promise';
-import { createCommaSeparatedColumns } from './utils/query-utils';
+
+function createInsertColumns(columns: string[]): string {
+  let columnsString = '';
+
+  for (let i = 0; i < columns.length - 1; ++i) {
+    columnsString += `\`${columns[i]}\`, `;
+  }
+  if (columns.length) {
+    columnsString += `\`${columns[columns.length - 1]}\``;
+  }
+
+  return columnsString;
+}
 
 function createCommaSeparatedQuestionMarks(count: number): string {
   let result = '';
@@ -15,18 +27,56 @@ function createCommaSeparatedQuestionMarks(count: number): string {
   return result;
 }
 
+function createEqualsSeparatedUpdateColumns(columns: string[]): string {
+  let columnString = '';
+
+  for (let i = 0; i < columns.length - 1; ++i) {
+    columnString += `\`${columns[i]}\` = ?, `;
+  }
+  if (columns.length) {
+    columnString += `\`${columns[columns.length - 1]}\` = ?`;
+  }
+
+  return columnString;
+}
+
 export function insert(
   connection: Connection,
   tableName: string,
   data: Record<string, object>
 ) {
-  const dataValues = Object.entries(data);
+  const dataValues = Object.values(data);
 
   connection.query(
-    `INSERT INTO ${tableName} (${createCommaSeparatedColumns(
-      tableName,
+    `INSERT INTO \`${tableName}\` (${createInsertColumns(
       Object.keys(data)
     )}) VALUES(${createCommaSeparatedQuestionMarks(dataValues.length)})`,
-    dataValues
+    [...dataValues]
   );
+}
+
+export function update(
+  connection: Connection,
+  tableName: string,
+  tableId: string,
+  rowId: number,
+  data: Record<string, object>
+) {
+  connection.query(
+    `UPDATE \`${tableName}\`` +
+      `SET ${createEqualsSeparatedUpdateColumns(Object.keys(data))}` +
+      `WHERE \`${tableId}\` = ?`,
+    [...Object.values(data), rowId]
+  );
+}
+
+export function remove(
+  connection: Connection,
+  tableName: string,
+  tableId: string,
+  rowId: number
+) {
+  connection.query(`DELETE FROM \`${tableName}\` WHERE \`${tableId}\` = ?`, [
+    rowId
+  ]);
 }
